65eb873a90c60ff47aba793d8cb4fa37e88730e8
Jordan Robinson <jordan@stateot.art>
2024-05-23 10:24:36 +1000

    Fixed type error on Insights page

--- src/routes/insights/+page.svelte
+++ src/routes/insights/+page.svelte
@@ -12,28 +12,28 @@
 	
 	const selectedTag = writable<string | null>('');
 	const pageNumber = writable<number | null>(1);
-	const lowerBound = writable<number | null>(1);
-	const upperBound = writable<number | null>(1);
 	const lastPage = writable<boolean>(false);
 	const firstPage = writable<boolean>(true);
+	const upperBound = writable<number>(0);
+	const lowerBound = writable<number>(0);
 
 	$: {
 		$selectedTag = $page.url.searchParams.get('tag') || '';
 		$pageNumber = parseInt($page.url.searchParams.get('page') || '1');
-		$lowerBound = (data.meta.pagination.page - 1) * data.meta.pagination.limit + 1;
-		$upperBound = Math.min(data.meta.pagination.page * data.meta.pagination.limit, data.meta.pagination.total);
-		$lastPage = $page.data.meta.pagination.page === $page.data.meta.pagination.pages;
-		$firstPage = $page.data.meta.pagination.page === 1;
+		$lastPage = data.meta.pagination.page === data.meta.pagination.pages;
+		$firstPage = data.meta.pagination.page === 1;
+		$lowerBound = data.meta.pagination.page === 1 ? 1 : (data.meta.pagination.page - 1) * data.meta.pagination.limit + 1;
+		$upperBound = data.meta.pagination.page === data.meta.pagination.pages ? data.meta.pagination.total : data.meta.pagination.page * data.meta.pagination.limit;
 	};
 
 	function nextPage() {
-		if (data.meta.pagination.page < data.meta.pagination.pages && $pageNumber !== null) {
+		if (!$lastPage && $pageNumber !== null) {
 			goto(`/insights?page=${$pageNumber + 1}&tag=${$selectedTag}`);
 		}
 	};
 
 	function prevPage() {
-		if (data.meta.pagination.page > 1 && $pageNumber !== null) {
+		if (!$firstPage && $pageNumber !== null) {
 			goto(`/insights?page=${$pageNumber - 1}&tag=${$selectedTag}`);
 		}
 	};
@@ -69,9 +69,9 @@
 
 <div class="flex flex-wrap justify-center py-8">
 	<div class="max-w-lg space-x-2 space-y-1 text-center">
-		<p>{$lowerBound} - {$upperBound} of {data.meta.pagination.total} Posts</p>
+		<p>{$lowerBound} - {$upperBound} of {data.meta.pagination.total}</p>
 		<div class="join grid grid-cols-2">
-			<button class="join-item btn btn-outline" class:btn-disabled={$firstPage} on:click={prevPage}>Previous page</button>
+			<button class="join-item btn btn-outline" class:btn-disabled={$firstPage} on:click={prevPage}>Previous</button>
 			<button class="join-item btn btn-outline" class:btn-disabled={$lastPage} on:click={nextPage}>Next</button>
 		</div>
 	</div>
