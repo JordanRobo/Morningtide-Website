02149162b1b7a825a31afba8641da9cc952d5709
JordanRobo <jordan.robinson@mercury.rs>
2025-12-22 17:17:19 +1100

    Chore: commiting before updating packages

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,16 +4,24 @@ import type { LayoutServerLoad } from "./$types";
 import { valibot } from "sveltekit-superforms/adapters";
 import { subscribeSchema } from "$lib/schema";
 
-export const load: LayoutServerLoad = async ({ url }) => {
+export const load: LayoutServerLoad = async ({ setHeaders }) => {
 	const subscribeForm = await superValidate(valibot(subscribeSchema));
 
-	try {
-		const [tags, highlight_posts, privacyPolicy] = await Promise.all([ghost?.tags?.browse(), ghost?.posts?.browse({ limit: 3 }), ghost?.pages?.read({ slug: "privacy-policy" })]);
+	setHeaders({
+		"cache-control": "public, max-age=300",
+	});
 
-		const { pathname } = url;
-		return { tags, highlight_posts, pathname, privacyPolicy, subscribeForm };
+	try {
+		const [tags, privacyPolicy] = await Promise.all([ghost?.tags?.browse(), ghost?.pages?.read({ slug: "privacy-policy" })]);
+		return { tags, privacyPolicy, subscribeForm };
 	} catch (error) {
 		console.error("Error loading layout data:", error);
-		return { tags: [], highlight_posts: [], pathname: url.pathname, privacyPolicy: null, subscribeForm };
+		console.error("Error details:", {
+			message: error instanceof Error ? error.message : "Unknown error",
+			stack: error instanceof Error ? error.stack : undefined,
+			timestamp: new Date().toISOString(),
+		});
+
+		return { tags: [], privacyPolicy: null, subscribeForm };
 	}
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,27 @@
 import { superValidate, message } from "sveltekit-superforms";
 import { valibot } from "sveltekit-superforms/adapters";
 import { fail } from "@sveltejs/kit";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { enquirySchema, subscribeSchema } from "$lib/schema";
 import { pb } from "$lib/pb.server";
+import { ghost } from "$lib/ghost";
+
+export const load: PageServerLoad = async ({ url }) => {
+	try {
+		const [highlight_posts] = await ghost?.posts?.browse({ limit: 3 });
+
+		return { highlight_posts, pathname: url.pathname };
+	} catch (error) {
+		console.error("Error loading pagedata", error);
+		console.error("Error details:", {
+			message: error instanceof Error ? error.message : "Unknown error",
+			stack: error instanceof Error ? error.stack : undefined,
+			timestamp: new Date().toISOString(),
+		});
+
+		return { highlight_posts: [], pathname: url.pathname };
+	}
+};
 
 export const actions: Actions = {
 	subscribe: async ({ request }) => {
